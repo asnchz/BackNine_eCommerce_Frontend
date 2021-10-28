b7189205190c49446a24660416118b5e4e299873
asnchz <asnchz27@yahoo.com>
2021-10-28 17:00:40 -0500

    Created login file

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,65 @@
 import React, {useState} from "react";
 import axios from axios;
+import { useHistory } from "react-router-dom";
 
-c
+
+const Login = (props) => {
+    const {setUserToken}= props;
+    const history = useHistory();
+
+    const userLogin = {
+        username:'',
+        password:''
+    }
+    const [loginInfo, setLoginInfo] = useState(userLogin);
+
+    const handleChange = event => {
+        setLoginInfo({...loginInfo, [event.target.name]: event.target.value});
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        login();
+    }
+
+    const login = async () =>{
+        let user = loginInfo;
+        let response = await axios.post("https://localhost:44394/api/authentication/login", user).catch(function(error){
+            if (error.response){
+                alert("Login is incorrect. Try again")
+            }
+        });
+        if (response !== undefined){
+            setUserToken(response.data.token)
+            history.push("/home")
+        }
+    }
+
+  return (
+      <div className={`${!props ? "active": ""} show`}>
+          <div className="login-form">
+              <div className="form-box solid">
+                  <form>
+                      <h1 className="login-text">Login</h1>
+                      <label>Username</label><br></br>
+                      <input
+                      type = "text"
+                      name = "username"
+                      className = "login-box"
+                      /><br></br>
+                      <label>Password</label><br></br>
+                      <input
+                      type = "password"
+                      name = "password"
+                      className = "login-box"
+                      /><br></br>
+                      <input type ="submit" value ="LOGIN" className ="login-btn"/>
+                  </form>
+              </div>
+          </div>
+
+      </div>
+  );
+}
+
+export default Login;
