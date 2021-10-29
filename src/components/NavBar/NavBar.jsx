c1757e6c56fc6a3ba4c4cb1693dea5cc107cb5fe
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-10-29 11:16:56 -0500

    Pair programmed with Jay. We installed react-bootstrap and utilized the built in NavBar tool. Will need to add the href links to get the buttons working.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./NavBar.css";
 import Login from "../Login/Login";
+import {Navbar} from "react-bootstrap";
+import {Container} from "react-bootstrap";
+import {Nav} from "react-bootstrap";
+import {NavDropdown} from "react-bootstrap";
 
 function NavBar(props) {
   let { setIsShowLogin } = props;
@@ -9,16 +13,37 @@ function NavBar(props) {
     setIsShowLogin((loginInfo) => !loginInfo);
   };
   return (
-    <div className="row row-space">
-      <div className="col-md-12" style={{ padding: 0 }}>
-        <div className="navbar">
-          <h1>BackNine</h1>
-          <span onClick={handleClick} className="loginicon">
-            Login
-          </span>
-        </div>
-      </div>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home"><h1>BackNine</h1></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Login</Nav.Link>
+            <Nav.Link href="#pricing">Sign-Up</Nav.Link>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Buy" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Clubs</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Balls</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Bags</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Tech</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.5">Apparel</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Sell" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/4.1">Clubs</NavDropdown.Item>
+              <NavDropdown.Item href="#action/4.2">Balls</NavDropdown.Item>
+              <NavDropdown.Item href="#action/4.3">Bags</NavDropdown.Item>
+              <NavDropdown.Item href="#action/4.4">Tech</NavDropdown.Item>
+              <NavDropdown.Item href="#action/4.5">Apparel</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link eventKey={2} href="#memes">
+              Cart
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 export default NavBar;
