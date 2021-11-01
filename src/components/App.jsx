d3a8ce3b137bed960dd391dfd555da52b7535117
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-11-01 11:39:40 -0500

    Pair programmed with Jay. We created the ProductList component and made it table the productroduct data.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import jwtDecode from "jwt-decode";
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
 import Register from "./Register/Register";
 import image from "../Image/HomePage.jpg";
 import axios from "axios";
+import ProductList from "./ProductList/ProductList";
 
->>>>>>> 32ea14c544c2ce6a20713bf62377bc59c0d9b740
+// >>>>>>> 32ea14c544c2ce6a20713bf62377bc59c0d9b740
 
 class App extends Component {
   state = {
-    loggedInUser: null,
+    // loggedInUser: null,
+    products: [],
   };
 
   componentDidMount() {
@@ -21,96 +24,70 @@ class App extends Component {
     } catch (error) {
       console.log(error);
     }
+
+    this.getProductCategory("Clubs");
   }
 
-  async getProductClubs() {
+  async getProductCategory(category) {
+    // category = "Clubs"
     try {
       let response = await axios.get(
-        "https://localhost:44394/api/product/Clubs"
+        `https://localhost:44394/api/product/${category}`
       );
       console.log(response.data);
       this.setState({
-        Clubs: response.data,
+        selectedCategoryData: response.data,
+        selectedCategory: category
       });
     } catch (ex) {
       alert("Error in API Call");
     }
   }
 
-  async getProductBalls() {
-    try {
-      let response = await axios.get(
-        "https://localhost:44394/api/product/Balls"
-      );
-      console.log(response.data);
-      this.setState({
-        Balls: response.data,
-      });
-    } catch (ex) {
-      alert("Error in API Call");
-    }
-  }
-
-  async getProductBags() {
-    try {
-      let response = await axios.get(
-        "https://localhost:44394/api/product/Bags"
-      );
-      console.log(response.data);
-      this.setState({
-        Bags: response.data,
-      });
-    } catch (ex) {
-      alert("Error in API Call");
-    }
-  }
-
-  async getProductApparel() {
-    try {
-      let response = await axios.get(
-        "https://localhost:44394/api/product/Apparel"
-      );
-      console.log(response.data);
-      this.setState({
-        Apparel: response.data,
-      });
-    } catch (ex) {
-      alert("Error in API Call");
-    }
-  }
-
-  async getProductTech() {
-    try {
-      let response = await axios.get(
-        "https://localhost:44394/api/product/Tech"
-      );
-      console.log(response.data);
-      this.setState({
-        Tech: response.data,
-      });
-    } catch (ex) {
-      alert("Error in API Call");
-    }
-  }
+  // async getProductCategory() {
+  //   try {
+  //     let response = await axios.get(
+  //       `https://localhost:44394/api/product/Clubs`
+  //     );
+  //     console.log(response.data);
+  //     this.setState({
+  //       selectedCategoryData: response.data,
+  //     });
+  //   } catch (ex) {
+  //     alert("Error in API Call");
+  //   }
+  // }
 
   render() {
     return (
       <div>
         <div
           style={{
-              backgroundImage: `url(${image})`,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-              position: "absolute",
-              height: "100vh",
-              width: "100vw",
-            }}
+            backgroundImage: `url(${image})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            position: "absolute",
+            height: "100vh",
+            width: "100vw",
+          }}
         >
           <NavBar />
-          <Register button type="button" data-toggle="modal" data-target="#Modal" />
+          <Register
+            button
+            type="button"
+            data-toggle="modal"
+            data-target="#Modal"
+          />
           <h1>The Place to Buy and Sell Golf Gear</h1>
-          <p className="front-page-header">The Place to Buy and Sell your Golf gear!</p>
+          <p className="front-page-header">
+            The Place to Buy and Sell your Golf gear!
+          </p>
+          {console.log("Potential prop data: ", this.state.selectedCategoryData )}
+          {this.state.selectedCategoryData != undefined && <ProductList products={this.state.selectedCategoryData} /> }
         </div>
+        <Switch>
+          <Route></Route>
+        </Switch>
       </div>
     );
   }
